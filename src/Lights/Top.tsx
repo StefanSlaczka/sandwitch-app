659cf216a28062273edf3c4830000ff37764bed6
StefanSlaczka <78507059+StefanSlaczka@users.noreply.github.com>
2022-05-15 14:50:40 -0400

    Update Top.tsx

--- src/Lights/Top.tsx
+++ src/Lights/Top.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
+import ReactDOM from "react-dom";
 
-function Top() {
+function Top(this: any) {
   const [tM, setTM] = useState("");
   let [ingrents, setIngrents] = useState([""]);
 
@@ -11,16 +12,60 @@ function Top() {
     setIngrents([...ingrents, ingrent]);
   }
 
-  const Reciept = () => {
-    return (
-      <form>
-        <h3>--Reciept--</h3>
-        <h4>{tM}</h4>
-        if(tM == "Turkey"){<h4>Total: 7.25</h4>}
-        else if(tM == "Turkey"){<h4>Total: 7.50</h4>}
-        else if(tM == "Turkey"){<h4>Total: 9.25</h4>}
-      </form>
+  /*
+   1. Make a fuction called done that will save the meat in a verable called meat
+   2. Set the price of the meat if it is Turkey the price is 7.25 if it is Ham the price is 7.50 if it is Chicken the price is 9.25
+   3. store the ingrents in the useState hook to a list called ing
+   4. sort the ing list to this [lettuce, tomato, onions, pickles, mayo]
+   5. then make a div with the meat, the price, the ingrents.
+ */
+  const done = () => {
+    const meat = tM;
+    let price = 0;
+    let ing = [...ingrents];
+
+    let newIng = [""];
+    //  [lettuce, tomato, onions, pickles, mayo]
+    if (ing.includes("Lettuce ")) {
+      newIng.push("Lettuce ");
+    }
+    if (ing.includes("Tomato ")) {
+      newIng.push("Tomato ");
+    }
+    if (ing.includes("Onions ")) {
+      newIng.push("Onions ");
+    }
+    if (ing.includes("Pickles ")) {
+      newIng.push("Pickles ");
+    }
+    if (ing.includes("Mayo ")) {
+      newIng.push("Mayo ");
+    }
+
+    if (meat === "Turkey") {
+      price = 7.25;
+    } else if (meat === "Ham") {
+      price = 7.5;
+    } else if (meat === "Stake") {
+      price = 9.25;
+    } else {
+      console.log(newIng);
+    }
+    if(tM !== ""){
+      ReactDOM.render(
+        <div className='py-6'>
+          <div className='text-center border-solid border-4 border-indigo-700	w-1/2 text-3xl mx-auto font-semibold '>
+            <h1>---Recipt---</h1>
+            <h2>
+              {meat} -- {price}
+            </h2>
+            <h3>Ingrents:</h3>
+            <h3>{newIng}</h3>
+          </div>
+        </div>,
+        document.getElementById("ok")
     );
+    }
   };
 
   return (
@@ -64,7 +109,7 @@ function Top() {
 
           <div className='text-center p-8'>
             <button
-              className='h-10 px-5 text-indigo-100 transition-colors duration-150 bg-indigo-700 focus:shadow-outline hover:bg-indigo-800'
+              className='h-10 px-5 text-indigo-100 transition-colors duration-150 bg-indigo-700 focus:shadow-outline hover:bg-indigo-800 rounded-full'
               onClick={() => setTM("")}
             >
               Reset
@@ -83,13 +128,13 @@ function Top() {
           </button>
           <button
             className='h-10 px-5 text-indigo-100 transition-colors duration-150 bg-indigo-700 focus:shadow-outline hover:bg-indigo-800'
-            onClick={() => addIngrent("Lettie ")}
+            onClick={() => addIngrent("Lettuce ")}
           >
             Lettie
           </button>
           <button
             className='h-10 px-5 text-indigo-100 transition-colors duration-150 bg-indigo-700 focus:shadow-outline hover:bg-indigo-800'
-            onClick={() => addIngrent("Pickels ")}
+            onClick={() => addIngrent("Pickles ")}
           >
             Pickels
           </button>
@@ -108,20 +153,20 @@ function Top() {
         </div>
         <div className='text-center p-8'>
           <button
-            className='h-10 px-5 text-indigo-100 transition-colors duration-150 bg-indigo-700 focus:shadow-outline hover:bg-indigo-800'
+            className='h-10 px-5 text-indigo-100 transition-colors duration-150 bg-indigo-700 focus:shadow-outline hover:bg-indigo-800 rounded-full'
             onClick={() => setIngrents([""])}
           >
             Reset
           </button>
         </div>
-        <div className="text-center">
-          <button className='Reciept h-10 px-5 text-indigo-100 transition-colors duration-150 bg-indigo-700 rounded-r-lg focus:shadow-outline hover:bg-indigo-800 '
-          onSubmit={Reciept}>
-            OrderNow -- comment future design
+        <div className='text-center pb-6'>
+          <button
+            className='rounded-full h-10 px-5 text-indigo-100 transition-colors duration-150 bg-indigo-700 focus:shadow-outline hover:bg-indigo-800 '
+            onClick={done}
+          >
+            OrderNow
           </button>
-          <form id="Reciept">
-
-          </form>
+          <form id='ok'></form>
         </div>
       </div>
     </React.Fragment>
